1dee8582754cbd60d0f57f13afaca1ec6694a27f
shi-ze.zheng <shi-ze.zheng@neuroncloud.ai>
2022-04-27 15:33:36 +0800

    vue3 + ant-design-vue + typescript

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <a-button type="primary" :loading='buttonLoading' @click="clickLoading">Primary Button</a-button>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive, defineComponent, toRefs } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+interface DataProps {
+  buttonLoading: boolean,
+  clickLoading: () => void,
+}
+export default defineComponent({
+  name: 'HomeView',
+  setup() {
+    const data:DataProps = reactive({
+      buttonLoading: false,
+      clickLoading: () => {
+        data.buttonLoading = true;
+        setTimeout(() => {
+          data.buttonLoading = false;
+        }, 2000);
+      }
+    });
+    const refData = toRefs(data);
+    return {
+      ...refData
+    }
+  },
+  components: {
+    HelloWorld,
+  },
+});
+</script>
